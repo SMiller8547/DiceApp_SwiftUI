d0a6f168e3b1fff05b287a0ba0a78839d6637497
SterlingM <miller8547@gmail.com>
2021-10-07 13:07:54 -0500

    Initial Commit

---
+++ DiceAppSwiftUI/DiceAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceAppSwiftUIApp.swift
+//  DiceAppSwiftUI
+//
+//  Created by Sterling Miller on 10/7/21.
+//
+
+import SwiftUI
+
+@main
+struct DiceAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
